40906716a99c660a47240ccf532109955970d477
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-05 12:47:38 +0000

    Adding tarfile member sanitization to extractall()

--- robocar/train_with_crop.py
+++ robocar/train_with_crop.py
@@ -70,7 +70,26 @@ def extractTubFromS3(bucket, tubname, input_path="/tmp/traindata"):
     
     channel_names = [ tubname ]
     with tarfile.open(name=None, mode="r:*", fileobj=tarfile_obj) as tarball:
-        tarball.extractall(input_path)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tarball, input_path)
         print(channel_names)
         training_paths = [ os.path.join(input_path, channel) for channel in channel_names ]
         model_loc = "/tmp/model/" + tubname + "-model"
